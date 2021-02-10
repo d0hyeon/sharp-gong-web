d25d88178183b99e6c73a1a02a1a14f3b29a9df5
d0hyeon <ehgus6887@gmail.com>
2021-02-11 02:56:21 +0900

    feat: regexp 상수 파일 및 변수 생성

---
+++ src/constants/pattern.ts
@@ -0,0 +1,3 @@
+export const REGEXP_PASSWORD: RegExp = /(?=.*\d)(?=.*[a-zA-Z]).{8,}$/;
+export const REGEXP_EMAIL: RegExp = /[a-zA-Z0-9]+\@[a-zA-Z]+\.[a-zA-Z]{2,3}/;
+export const REGEXP_PHONE: RegExp = /^\d{3}\-?\d{4}\-?\d{4}$/;
